9d9d5a8cd47d03903d7c4b747bd6820da09dddac
DemanPierre <leomarcouf@hotmail.com>
2018-06-03 16:07:03 +0200

    reglage

--- js/engine.js
+++ js/engine.js
@@ -174,7 +174,10 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
-        'images/char-horn-girl.png'
+        'images/char-horn-girl.png',
+     
+
+       
     ]);
     Resources.onReady(init);
 
